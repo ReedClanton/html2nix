2ee74acbbae87f30ac7dd9ad36cd7068841d5083
reedclanton <clantonreed@gmail.com>
2024-02-24 11:54:05 -0600

    Fixed Bugs

--- Html2Nix.py
+++ Html2Nix.py
@@ -1,27 +1,26 @@
 from NetscapeBookmarksFileParser import *
 from NetscapeBookmarksFileParser import parser
 
+
 class Html2Nix:
-	def __init__(self, input_path: str = "./bookmarks.html", indent: int = 0, indent_size: int = 2) -> None:
+	def __init__(self, input_path: str = "./bookmarks.html", indent_size: int = 2) -> None:
 		'''
 		Converts `NETSCAPE bookmark file HTML https://discourse.nixos.org/t/error-reading-symbolic-link-nix-var-nix-profiles-per-user-root-channels-no-such-file-or-directory/34838https://learn.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/platform-apis/aa753582(v=vs.85)`_
 		to Nix syntax accepted by Home Manager's
-		`programs.firefox.profiles.<name>.bookmarks https://discourse.nixos.org/t/error-reading-symbolic-link-nix-var-nix-profiles-per-user-root-channels-no-such-file-or-directory/34838https://nix-community.github.io/home-manager/options.xhtml#opt-programs.firefox.profiles._name_.bookmarks`_.
-
+		`programs.firefox.profiles.<name>.bookmarks https://discourse.nixos.org/t/error-reading-symbolic-link-nix-var-nix-profiles-per-user-root-channels-no-such-file-or-directory/34838https://nix-community.github.io/home-manager/options.xhtml#opt-programs.firefox.profiles._name_.bookmarks`_.			   
 		:param input_path: Path to HTML file exported from brower including file name.
 		:type input_path: str
-		:param indent: Number of indents least indented line should have.
-		:type indent: int
 		:param indent_size: Number of space(s) each indent should represent.
 		:type indent_size: int
 		'''
 		self.input_path = input_path
-		self.indent = indent
 		self.indent_size = indent_size
 
-		# Convert.
-		self.data = self.convert()
-	
+		with open(self.input_path, "r") as file:
+			bookmarks = NetscapeBookmarksFile(file).parse()
+
+		self.nix = self.to_nix(self.convert(bookmarks.bookmarks.items))
+
 	@property
 	def input_path(self) -> str:
 		'''
@@ -42,30 +41,7 @@ class Html2Nix:
 		'''
 		# TODO: Ensure provided value is a valid path to a file.
 		self._input_path = path
-	
-	@property
-	def indent(self) -> int:
-		'''
-		TODO
 
-		:return: TODO
-		:rtype: int
-		'''
-		return self._indent
-
-	@indent.setter
-	def indent(self, indent: int) -> None:
-		'''
-		TODO
-
-		:param indent: TODO
-		:type indent: int
-		:raises ValueError: Occurs when `indent` value is negative.
-		'''
-		if indent < 0:
-			raise ValueError(f"'{indent}' is an invalid indent because it's negative.")
-		self._indent = indent
-	
 	@property
 	def indent_size(self) -> int:
 		'''
@@ -86,9 +62,9 @@ class Html2Nix:
 		'''
 		if indent_size < 1:
 			raise ValueError(f"'{indent_size}' is an invalid indent size because it's less not positive.")
-		self._indent_size
+		self._indent_size = indent_size
 
-	def convert_shortcut(shortcut: BookmarkShortcut) -> dict():
+	def convert_shortcut(self, shortcut: BookmarkShortcut) -> dict():
 		'''
 		Converts provided :py:class:`BookmarkShortcut` object into a dictionary.
 
@@ -108,7 +84,7 @@ class Html2Nix:
 			"url": shortcut.href
 		}
 
-	def convert_folder(folder: BookmarkFolder) -> dict():
+	def convert_folder(self, folder: BookmarkFolder) -> dict():
 		'''
 		Converts provided :py:class:`BookmarkFolder` object into a dictionary.
 
@@ -121,44 +97,45 @@ class Html2Nix:
 			return {
 				"name": folder.name.replace('"', ''),
 				"toolbar": folder.personal_toolbar,
-				"bookmarks": convert_bookmarks(folder.items)
+				"bookmarks": self.convert(folder.items)
 			}
 		return {
 			"name": folder.name.replace('"', ''),
-			"bookmarks": convert_bookmarks(folder.items)
+			"bookmarks": self.convert(folder.items)
 		}
 
-	def convert(self) -> list():
+	def convert(self, bookmarks: list()) -> list():
 		'''
-		Reads in bookmarks HTML file and uses :py:class:`NetscapeBookmarkFile` to convert bookmarks into native Python variables contained in a list.
+		Converts :py:class:`NetscapeBookmarkFile` object into native Python variables contained in a list.
 
+		:param bookmarks: Netscape bookmarks HTML file contents as parsed by :py:class:`NetscapeBookmarkFile`.
+		:type bookmarks: list()
 		:return: List of shortcuts and folder from bookmark HTML file stored in Python native variables in a list.
 		:rtype: list()
 		'''
-		with open(self.input_path, "r") as file:
-			bookmarks = NetscapeBookmarksFile(file).parse()
-
 		# Stores and returns converted bookmark data.
 		rt_b = list()
 
-		for b in bookmarks.bookmarks.items:
+		for b in bookmarks:
 			if isinstance(b, BookmarkShortcut) and b.name != "Recent Tags":
 				rt_b.append(self.convert_shortcut(b))
 			elif isinstance(b, BookmarkFolder):
 				rt_b.append(self.convert_folder(b))
-	
+
 		return rt_b
 
-	def shortcut_to_nix(self, shortcut: dict()) -> str:
+	def shortcut_to_nix(self, shortcut: dict(), indent: int) -> str:
 		'''
 		Converts provided shortcut data to Nix formatted string.
 
 		:param shortcut: Shortcut data to be converted to Nix string.
 		:type shortcut: dict()
+		:param indent: Starting indent of returned Nix syntax in number of tabs.
+		:type indent: int
 		:return: Provided shortcut as Nix syntax formatted string.
 		:rtype: str
 		'''
-		tab = "\t" * self.indent
+		tab = "\t" * indent
 
 		# Name.
 		msg = f"{tab}\tname = \"{shortcut['name']}\";\n"
@@ -174,16 +151,18 @@ class Html2Nix:
 
 		return f"{tab}" + "{\n" + msg + f"{tab}" + "}\n"
 
-	def folder_to_nix(self, folder: dict()) -> str:
+	def folder_to_nix(self, folder: dict(), indent: int) -> str:
 		'''
 		Converts provided folder data to Nix formatted string.
 
 		:param folder: Folder data to be converted to Nix string.
 		:type folder: dict()
+		:param indent: Starting indent of returned Nix syntax in number of tabs.
+		:type indent: int
 		:return: Provided folder as Nix syntax formatted string.
 		:rtype: str
 		'''
-		tab = "\t" * self.indent
+		tab = "\t" * indent
 
 		# Name.
 		msg = f"{tab}\tname = \"{folder['name']}\";\n"
@@ -192,28 +171,34 @@ class Html2Nix:
 			msg += f"{tab}\ttoolbar = {str(folder['toolbar']).lower()};\n"
 		# Folder contents.
 		msg += f"{tab}\tbookmarks = [\n"
-		msg += to_nix(folder["bookmarks"], indent+2)
+		msg += self.to_nix(folder["bookmarks"], indent+2)
 
 		return f"{tab}" + "{\n" + msg + f"{tab}\t];\n" + f"{tab}" + "}\n"
 
-	def to_nix(self) -> str:
+	def to_nix(self, bookmarks: list(), indent: int = 0) -> str:
 		'''
 		Converts provided list of bookmarks to Nix formatted text.
 
+		:param bookmarks: List of bookmarks caller would like converted to Nix syntax.
+		:type bookmarks: list()
+		:param indent: Starting indent of returned Nix syntax in number of tabs.
+		:type indent: int
 		:return: Provided bookmark list as a Nix syntax formatted string.
 		:rtype: str
 		'''
 		rt_nix = ""
 
-		for b in self.data:
+		for b in bookmarks:
 			if "url" in b.keys():
-				rt_nix += shortcut_to_nix(b)
+				rt_nix += self.shortcut_to_nix(b, indent)
 			else:
-				rt_nix += folder_to_nix(b)
+				rt_nix += self.folder_to_nix(b, indent)
 	
 		return rt_nix
 
 
+
 html2nix = Html2Nix("./input/bookmarks.html")
-print(html2nix.to_nix())
+print(html2nix.nix)
+
 
